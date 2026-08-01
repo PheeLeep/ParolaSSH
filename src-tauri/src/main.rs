7debc7c97b55a89a7027da264823b1f921485245
PheeLeep <ggprogrammin@gmail.com>
2026-08-01 11:52:31 +0800

    feat: add conditional removal of SESSION_MANAGER variable for Linux

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+
+    #[cfg(target_os = "linux")]
+    std::env::remove_var("SESSION_MANAGER");
+
     parolassh_lib::run()
 }
